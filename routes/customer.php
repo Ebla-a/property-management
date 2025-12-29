83f493fd0d801ddce7d0480d16b2026c8ddedfe2
hasan123dayoub-del <hasan123dayoub@gmail.com>
2025-12-29 16:17:26 +0300

    Revert "Raising the first amendments"

--- routes/customer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Services\RatingService;
-
-Route::middleware(['auth', 'role:customer'])->group(function () {
-    Route::post('/ratings', [RatingService::class, 'store'])
-        ->name('customer.ratings.store');
-});
